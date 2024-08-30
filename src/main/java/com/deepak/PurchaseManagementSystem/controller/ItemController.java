c10458ed7c7d292f44e248ef65a62b59d8fed9a6
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-08-31 02:23:32 +0545

    feat(Item): create fetchAllItem and DeleteItemById using ItemService

--- src/main/java/com/deepak/PurchaseManagementSystem/controller/ItemController.java
+++ src/main/java/com/deepak/PurchaseManagementSystem/controller/ItemController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/item")
 public class ItemController {
@@ -24,10 +26,25 @@ public class ItemController {
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
+
     @GetMapping("/fetchById/{id}")
     public ResponseEntity<ApiResponse> getItemById(@PathVariable long id){
         ItemDto itemDto = itemService.getItemById(id);
         ApiResponse response = new ApiResponse(HttpStatus.OK.value(),itemDto);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @GetMapping("/fetchAllItem")
+    public ResponseEntity<ApiResponse> getAllItems(){
+        List<ItemDto> itemDto = itemService.getAllItems();
+        ApiResponse response = new ApiResponse(HttpStatus.OK.value(), itemDto);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse> deleteItemById(@PathVariable Long id){
+        itemService.deleteItem(id);
+        ApiResponse response = new ApiResponse(HttpStatus.OK.value(), "Deleted successfully");
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
